5c5d5b5bc7d5726e7d6b402eb8138a2c99370222
Alex <format37@gmail.com>
2024-03-25 13:43:55 +0400

    modified:   README.md 	modified:   run.sh

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gptaction
 Chat-GPT actions server example
+#### Installation
+* Open the 8080 https port on your firewall
+```
+git clone https://github.com/format37/gptaction.git
+cd gptaction
+python3 -m pip install -r requirements.txt
+sh run.sh
+```
--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-uvicorn app:app --port 8080 --host 0.0.0.0 --reload --workers 1
+uvicorn app:app \
+    --port 8080 \
+    --host 0.0.0.0 \
+    --ssl-certfile /etc/letsencrypt/live/rtlm.info/fullchain.pem \
+    --ssl-keyfile /etc/letsencrypt/live/rtlm.info/privkey.pem \
+    --reload \
+    --workers 1
